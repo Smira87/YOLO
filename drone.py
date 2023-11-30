44b2111d9a29bc5a837167fe781fca4558f5340a
Serhii Smirnov <snoff87@gmail.com>
2023-11-30 17:26:30 +0200

    Diagonal flight

--- drone.py
+++ drone.py
@@ -12,9 +12,6 @@ import time
 
 SendInput = ctypes.windll.user32.SendInput
 
-close_target_x = 999
-close_target_y = 999
-
 E = 0x12
 W = 0x11
 A = 0x1E
@@ -34,7 +31,6 @@ DIR_DOWN = 0xD0
 
 # C struct redefinitions
 PUL = ctypes.POINTER(ctypes.c_ulong)
-
 
 class KeyBdInput(ctypes.Structure):
     _fields_ = [("wVk", ctypes.c_ushort),
@@ -83,6 +79,7 @@ def go(dir, sec):
         time.sleep(sec)
         ReleaseKey(S)
 
+
 def turn(dir, sec):
     if dir == "right":
         PressKey(H)
@@ -94,23 +91,9 @@ def turn(dir, sec):
         ReleaseKey(G)
 
 
-def level_1():
-    go("forward", 1.7)
-    go("backwards", 1)
-    time.sleep(32)
-    go("forward", 1.1)
-    turn("left", 0.7)
-    
-    turn("right", 0.6)
-    go("forward", 4)
-    PressKey(E)
-    go("forward", 3)
-
-
 def action_fire():
     PressKey(K)
     ReleaseKey(K)
-
 
 def action_left():
     PressKey(A)
@@ -121,20 +104,23 @@ def action_right():
     PressKey(D)
     ReleaseKey(D)
 
+
 def action_up():
     PressKey(W)
     ReleaseKey(W)
 
+
 def action_down():
     PressKey(S)
     ReleaseKey(S)
+
+
 def PressKey(hexKeyCode):
     extra = ctypes.c_ulong(0)
     ii_ = Input_I()
     ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra))
     x = Input(ctypes.c_ulong(1), ii_)
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
 
 def ReleaseKey(hexKeyCode):
     extra = ctypes.c_ulong(0)
@@ -160,20 +146,7 @@ def box_label(image, box, label='', color=(128, 128, 128), txt_color=(255, 255, 
 def plot_bboxes(image, boxes, labels=[], colors=[], score=True, conf=None):
     # Define COCO Labels
     if labels == []:
-        labels = {0: u'undefined', 1: u'person', 2: u'bicycle', 3: u'car', 4: u'motorcycle', 5: u'airplane', 6: u'bus',
-                  7: u'train', 8: u'truck', 9: u'boat', 10: u'traffic light', 11: u'fire hydrant', 12: u'stop sign',
-                  13: u'parking meter', 14: u'bench', 15: u'bird', 16: u'cat', 17: u'dog', 18: u'horse', 19: u'sheep',
-                  20: u'cow', 21: u'elephant', 22: u'bear', 23: u'zebra', 24: u'giraffe', 25: u'backpack',
-                  26: u'umbrella', 27: u'handbag', 28: u'tie', 29: u'suitcase', 30: u'frisbee', 31: u'skis',
-                  32: u'snowboard', 33: u'sports ball', 34: u'kite', 35: u'baseball bat', 36: u'baseball glove',
-                  37: u'skateboard', 38: u'surfboard', 39: u'tennis racket', 40: u'bottle', 41: u'wine glass',
-                  42: u'cup', 43: u'fork', 44: u'knife', 45: u'spoon', 46: u'bowl', 47: u'banana', 48: u'apple',
-                  49: u'sandwich', 50: u'orange', 51: u'broccoli', 52: u'carrot', 53: u'hot dog', 54: u'pizza',
-                  55: u'donut', 56: u'cake', 57: u'chair', 58: u'couch', 59: u'potted plant', 60: u'bed',
-                  61: u'dining table', 62: u'toilet', 63: u'tv', 64: u'laptop', 65: u'mouse', 66: u'remote',
-                  67: u'keyboard', 68: u'cell phone', 69: u'microwave', 70: u'oven', 71: u'toaster', 72: u'sink',
-                  73: u'refrigerator', 74: u'book', 75: u'clock', 76: u'vase', 77: u'scissors', 78: u'teddy bear',
-                  79: u'hair drier', 80: u'toothbrush'}
+        labels = {1: u'person'}
     # Define colors
     if colors == []:
         # colors = [(6, 112, 83), (253, 246, 160), (40, 132, 70), (205, 97, 162), (149, 196, 30), (106, 19, 161), (127, 175, 225), (115, 133, 176), (83, 156, 8), (182, 29, 77), (180, 11, 251), (31, 12, 123), (23, 6, 115), (167, 34, 31), (176, 216, 69), (110, 229, 222), (72, 183, 159), (90, 168, 209), (195, 4, 209), (135, 236, 21), (62, 209, 199), (87, 1, 70), (75, 40, 168), (121, 90, 126), (11, 86, 86), (40, 218, 53), (234, 76, 20), (129, 174, 192), (13, 18, 254), (45, 183, 149), (77, 234, 120), (182, 83, 207), (172, 138, 252), (201, 7, 159), (147, 240, 17), (134, 19, 233), (202, 61, 206), (177, 253, 26), (10, 139, 17), (130, 148, 106), (174, 197, 128), (106, 59, 168), (124, 180, 83), (78, 169, 4), (26, 79, 176), (185, 149, 150), (165, 253, 206), (220, 87, 0), (72, 22, 226), (64, 174, 4), (245, 131, 96), (35, 217, 142), (89, 86, 32), (80, 56, 196), (222, 136, 159), (145, 6, 219), (143, 132, 162), (175, 97, 221), (72, 3, 79), (196, 184, 237), (18, 210, 116), (8, 185, 81), (99, 181, 254), (9, 127, 123), (140, 94, 215), (39, 229, 121), (230, 51, 96), (84, 225, 33), (218, 202, 139), (129, 223, 182), (167, 46, 157), (15, 252, 5), (128, 103, 203), (197, 223, 199), (19, 238, 181), (64, 142, 167), (12, 203, 242), (69, 21, 41), (177, 184, 2), (35, 97, 56), (241, 22, 161)]
@@ -192,8 +165,11 @@ def plot_bboxes(image, boxes, labels=[], colors=[], score=True, conf=None):
                   (205, 137, 244), (193, 137, 224), (36, 152, 214), (17, 50, 238), (154, 165, 67), (114, 129, 60),
                   (119, 24, 48), (73, 8, 110)]
 
-    int_label = 0
 
+    min_dist_to_target = 999
+    global picked_box, picked_target_x, picked_target_yaw
+    picked_target_x = picked_target_y = 0
+    picked_box = None
     # plot each boxes
     for box in boxes:
         # add score in label if score=True
@@ -205,74 +181,50 @@ def plot_bboxes(image, boxes, labels=[], colors=[], score=True, conf=None):
 
         int_label = int(box[-1]) + 1
 
-        print(int_label)
+        print('============')
+        target_x = int(box[0]) + (int(box[2]) - int(box[0])) / 2
+        target_y = int(box[1]) + (int(box[3]) - int(box[1])) / 2
+        target_x = int(round(target_x))
+        target_y = int(round(target_y))
+        dx = abs(target_x - 300)
+        dy = abs(target_y - 225)
+        cur_dist_to_target = math.sqrt(dx ** 2 + dy ** 2)
+        if cur_dist_to_target < min_dist_to_target:
+            min_dist_to_target = cur_dist_to_target
+            picked_target_x = target_x
+            picked_target_y = target_y
+            picked_box = box
 
-        if (int_label == 1):  # Traffic Light
-            print('============')
-            target_x = int(box[0]) + (int(box[2]) - int(box[0])) / 2
-            target_y = int(box[1]) + (int(box[3]) - int(box[1])) / 2
-            target_x = int(round(target_x))
-            target_y = int(round(target_y))
-            target = (target_x, target_y)
-            cv2.rectangle(image, (target_x, target_y), (target_x + 4, target_y + 4), (255, 50, 50), thickness=2,
-                          lineType=cv2.LINE_AA)
-            print(target_x)
-            print("Y =" + str(target_y))
-            global action
-            global close_target_x
-            global close_target_y
+    cv2.rectangle(image, (picked_target_x, picked_target_y), (picked_target_x + 4, picked_target_y + 4),
+                    (255, 50, 50), thickness=2,
+                    lineType=cv2.LINE_AA)
+
+    global action
             # 800 x 600
 
-            cv2.rectangle(image, (300, 225), (300 + 4, 225 + 4), (255, 255, 50), thickness=2,
-                          lineType=cv2.LINE_AA)
+    cv2.rectangle(image, (300, 230), (300 + 4, 230 + 4), (255, 255, 50), thickness=2,
+                    lineType=cv2.LINE_AA)
+    if (picked_target_x >= 305):
+        action_right()
+    elif (picked_target_x <= 295):
+        action_left()
+    if (picked_target_y <= 230):
+        action_up()
+    elif (picked_target_y >= 240):
+        action_down()
+    if (
+            picked_target_x > 290 and picked_target_x < 310 and picked_target_y > 225 and picked_target_y < 240):  # 370 = center (Need to Calibrate!)
+        action_fire()
 
-            dx = abs(300 - target_x)
-            dy = abs(225 - target_y)
-            close_dx = abs(300 - close_target_x)
-            close_dy = abs(225 - close_target_y)
-
-            d_target = math.sqrt(dx ** 2 + dy ** 2)
-            close_d_target = math.sqrt(close_dx ** 2 + close_dy ** 2)
-            print("--------------------")
-            print(d_target)
-            print(close_d_target)
-            print("--------------------")
-            if d_target > close_d_target:
-                target_x = close_target_x
-                target_y = close_target_y
-            else:
-                close_target_x = target_x
-                close_target_y = target_y
-
-            '''dx = abs(300 - target_x)
-            dy = abs(225 - target_y)'''
-
-            if (abs(dx) >= abs(dy)):
-                if (target_x >= 305):
-                    action = 'right'
-                elif (target_x <= 295):
-                    action = 'left'
-            else:
-                if (target_y <= 220):
-                    action = 'up'
-                elif (target_y >= 230):
-                    action = 'down'
-            if (target_x > 290 and target_x < 310 and target_y > 215 and target_y < 235):  # 370 = center (Need to Calibrate!)
-                action = 'fire'
-                close_target_x = close_target_y = 999
-
-            print('============')
-
+    print('============')
+    if picked_box is not None:
         if conf:
-            if box[-2] > conf:
-                color = colors[int(box[-1])]
-                box_label(image, box, label, color)
+            if picked_box[-2] > conf:
+                color = colors[int(picked_box[-1])]
+                box_label(image, picked_box, label, color)
         else:
-            color = colors[int(box[-1])]
-            box_label(image, box, label, color)
-
-    if (int_label == 0):
-        action = ''
+            color = colors[int(picked_box[-1])]
+            box_label(image, picked_box, label, color)
 
 
 model = YOLO('best.pt')
@@ -310,21 +262,6 @@ def video_loop():
         plot_bboxes(frame, results[0].boxes.data)
 
         # ===================================
-
-        if (action == 'fire'):
-            action_fire()
-
-        if (action == 'right'):
-            action_right()
-
-        if (action == 'left'):
-            action_left()
-
-        if (action == 'up'):
-            action_up()
-
-        if (action == 'down'):
-            action_down()
 
         # Optional: Display the recording screen
         cv2.imshow('Live', frame)
